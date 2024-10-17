b5d94c60824e309ce0b3ee40dc09bd336b09c2d9
Michael Morten Sonne <michael@sonnes.cloud>
2024-10-17 14:44:28 +0200

    Add

---
+++ src/Globals.ps1
@@ -0,0 +1,52 @@
+﻿#--------------------------------------------
+# Declare Global Variables and Functions here
+#--------------------------------------------
+
+
+#Sample function that provides the location of the script
+function Get-ScriptDirectory
+{
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	[OutputType([string])]
+	param ()
+	if ($null -ne $hostinvocation)
+	{
+		Split-Path $hostinvocation.MyCommand.path
+	}
+	else
+	{
+		Split-Path $script:MyInvocation.MyCommand.Path
+	}
+}
+
+#Sample variable that provides the location of the script
+[string]$ScriptDirectory = Get-ScriptDirectory
+
+
+
+function Check-Modules
+{
+	$requiredModules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Applications")
+	
+	foreach ($module in $requiredModules)
+	{
+		if (-not (Get-Module -ListAvailable -Name $module))
+		{
+			$textboxLog.Text += "Module $module is not installed. Installing...`n"
+			Install-Module -Name $module -Force -Scope CurrentUser
+		}
+		else
+		{
+			$textboxLog.Text += "Module $module is already installed.`n"
+		}
+	}
+}
