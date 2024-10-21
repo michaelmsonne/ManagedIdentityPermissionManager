596649082bde0547b8b61d7de566a22b86de6ba4
Michael Morten Sonne <49366751+michaelmsonne@users.noreply.github.com>
2024-10-21 21:57:15 +0200

    Add new parts of the log functions

--- src/Globals.ps1
+++ src/Globals.ps1
@@ -13,6 +13,95 @@ $global:ToolName = "Managed Identity Permission Manager"
 $global:AuthorEmail = ""
 $global:AuthorCompany = "Sonne´s Cloud"
 
+#Logfile path
+$LogPath = "$Env:USERPROFILE\AppData\Local\$global:ToolName"
+
+#CheckLogPath function
+Function CheckLogPath
+{
+<#
+	.SYNOPSIS
+		CheckLogPath returns the value if logfile path exits or not.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+	try
+	{
+		$FolderName = $LogPath
+		if (Test-Path $FolderName)
+		{
+			#Write to logfile if exists
+			Write-Log -Level INFO -Message "The application log path exists: $LogPath"
+		}
+		else
+		{
+			#Create logfile of not exists
+			New-Item $FolderName -ItemType Directory
+			Write-Log -Level INFO -Message "The application log path does not exists and is created: $LogPath"
+		}
+	}
+	# Catch specific types of exceptions thrown by one of those commands
+	catch [System.Exception]
+	{
+		Write-Log -Level ERROR -Message $($Error[0].Exception.Message)
+	}
+	# Catch all other exceptions thrown by one of those commands
+	catch
+	{
+		Write-Log -Level ERROR -Message $($Error[0].Exception.Message)
+	}
+}
+
+#Logfile write log function
+Function Write-Log
+{
+<#
+	.SYNOPSIS
+		Save the information to specified logfile
+	
+	.DESCRIPTION
+		A detailed description of the Write-Log function.
+	
+	.PARAMETER Level
+		Set the information level in the logfile.
+	
+	.PARAMETER Message
+		The message to be logged in the logfile
+	
+	.PARAMETER logfile
+		The selected logfile to write to (there is a default logfile)
+	
+	.EXAMPLE
+		PS C:\> Write-Log -Level INFO -Message 'value1'
+	
+	.NOTES
+		Additional information about the function.
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $False)]
+		[ValidateSet("INFO", "WARN", "ERROR", "FATAL", "DEBUG")]
+		[String]$Level = "INFO",
+		[Parameter(Mandatory = $True)]
+		[string]$Message,
+		[Parameter(Mandatory = $False)]
+		[string]$logfile = "$LogPath\$($ToolName)_Log_$($env:computername)" + "_" + (Get-Date -Format "dd/MM/yyyy") + ".log"
+	)
+	
+	$Stamp = (Get-Date).toString("dd/MM/yyyy HH:mm:ss")
+	$Line = "$Stamp : $Level : $UserName : $Message"
+	If ($logfile)
+	{
+		Add-Content $logfile -Value $Line
+	}
+	
+	#HOW TO ADD A LOG ENTRY: Write-Log -Level INFO -Message "The application is started"
+}
+
 function Show-InputBox
 {
 	param
